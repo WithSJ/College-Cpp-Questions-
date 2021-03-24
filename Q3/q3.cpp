0ff71d7acf5ccf6fb5bd589d9ed601b690d04a72
Sandeep Jadam <withsj.in@gmail.com>
2021-03-24 13:15:42 +0530

    q3 improve output

--- Q3/q3.cpp
+++ Q3/q3.cpp
@@ -10,13 +10,21 @@ int main(int argc, char const *argv[])
 
     int total=0;
     
-    for (int i = 0; i <= num; i++)
+    for (int i = 1; i <= num && i > 0; i++)
     {
+        int sub_total = 0;
+        cout<<endl;
         for (int j = 1; j <= i; j++)
-        {total += j;}
+        {   cout<<j;
+            sub_total += j;
+            total += j;
+            if(j<i)
+            {cout<<" + ";}
+        }
+        cout<<" = ["<<sub_total<<"]";
     }
     
-    cout<<"\n"<<total<<endl;
-    
+    cout<<"\nThe Sum of above series : ["<<total<<"]"<<endl;
+
     return 0;
 }
